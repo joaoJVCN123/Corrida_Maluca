427c2dbee431df3ebf4553cb70dda27eb5f43001
Luiz_Felipe <luizfbserpa23@gmail.com>
2024-03-06 08:51:27 -0300

    final

---
+++ corrida_class.js
@@ -0,0 +1,13 @@
+class Obj{
+    des_obj(){
+    
+    }
+    
+}
+
+class Carro2 extends Carro{
+    atualiza_carro2(){
+    }
+    recomeca(){
+    }
+}
---
+++ corrida_main.js
@@ -0,0 +1,3 @@
+let t1 = new Text()
+let t2 = new Text()
+let t3 = new Text()
